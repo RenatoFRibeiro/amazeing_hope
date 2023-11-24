3549f2214cf808854979877f1f6f38c163ef3b0a
Renato Ribeiro <1190126@isep.ipp.pt>
2023-11-24 14:35:20 +0000

    player movement and asset enhanced

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject player;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float minZ;
+    public float maxZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ)), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
