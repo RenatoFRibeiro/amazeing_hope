1e723622063cd039cc42b93aeaf227077cf42a80
Renato Ribeiro <1190126@isep.ipp.pt>
2023-12-27 14:27:43 +0000

    Added textures to level, and added pause menu with quit, main menu and resume buttons!

--- Assets/Game_Scripts/MazeNode.cs
+++ Assets/Game_Scripts/MazeNode.cs
@@ -37,13 +37,16 @@ public class MazeNode : MonoBehaviour
         switch (state)
         {
             case NodeState.Available:
-                floor.material.color = Color.white;
+                print("No need.");
+                //floor.material.color = Color.white;
                 break;
             case NodeState.Current:
-                floor.material.color = Color.yellow;
+                print("No need.");
+                //floor.material.color = Color.yellow;
                 break;
             case NodeState.Completed:
-                floor.material.color = Color.blue;
+                print("No need.");
+                //floor.material.color = Color.blue;
                 break;
         }
     }
---
+++ Assets/Game_Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused = false;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        print("Resume");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        print("Return to Main Menu");
+        Time.timeScale = 1f;
+        isPaused = false;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        print("Quit");
+        Application.Quit();
+    }
+}
