9719be20986a583f24c2eb95e173c8895ebc9a8c
Renato Ribeiro <1190126@isep.ipp.pt>
2024-01-03 10:29:18 +0000

    s

--- Assets/Game_Scripts/Main_menu.cs
+++ Assets/Game_Scripts/Main_menu.cs
@@ -8,7 +8,7 @@ public class Main_menu : MonoBehaviour
         Debug.Log("Play!");
         SceneManager.LoadScene("Level1");
         Time.timeScale = 1f;
-        isPaused = false;
+        PauseMenu.isPaused = false;
     }
     public void QuitGame()
     {
